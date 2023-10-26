0b7b608b31567d89faf159b988ddf09a27c4cbd5
akinayturan <akinayturan@gmail.com>
2023-10-26 10:24:28 +0300

    Move Topbar content to Header and update links
    Deleted Topbar.js and relocated its content to Header.tsx. Also updated the href in the 'ISSN' link and 'MAKE A SUBMISSION' link

--- layout/Header/Header.tsx
+++ layout/Header/Header.tsx
@@ -6,13 +6,14 @@ export default function Header() {
 
   return (
     <header className={`header-area `}>
+
       <div className="topbar-one">
         <div className="container">
           <div className="topbar-one__left">
-            <a target="_blank" href="https://portal.issn.org/resource/issn/2618-6128">ISSN-2618-6128</a>
+            <a target="_blank" href="https://portal.issn.org/resource/issn/2636-7661">ISSN-2636-7661</a>
           </div>
           <div className="topbar-one__right">
-            <a href="https://dergipark.org.tr/en/journal/2661/submission/step/manuscript/new">MAKE A SUBMISSION</a>
+            <a href="https://dergipark.org.tr/en/journal/759/submission/step/manuscript/new">MAKE A SUBMISSION</a>
           </div>
         </div>
       </div>
